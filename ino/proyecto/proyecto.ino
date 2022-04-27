4a52da18b0d244be60374e7f8830f16564d0c822
Leo-Zubiri <leo.zubiri@outlook.com>
2022-04-27 00:17:02 -0500

    INO 2.0 FALTA LEER LED A ENCENDER

--- ino/proyecto/proyecto.ino
+++ ino/proyecto/proyecto.ino
@@ -1,11 +1,11 @@
 int i;
-const int leds[8] = {6, 7, 8, 9, 10, 11, 12, 13};
-const int ptms[8] = {A0,A1,A2,A3,A4,A5,A6,A7};
+const int leds[5] = {2,3,4,5,6};
+const int ptms[13] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12};
 String lectura;
 
 void setup() {
   // put your setup code here, to run once:
-    for (i  = 0; i < 8; i++) {
+    for (i  = 0; i < 5; i++) {
     pinMode(leds[i], OUTPUT);
   }
 
@@ -16,7 +16,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   lectura = "";
 
-  for(i=0; i< 7;i++){
+  for(i=0; i < 12;i++){
     lectura+= String(analogRead(ptms[i]))+",";
   }
   lectura+= String(analogRead(ptms[i]));
